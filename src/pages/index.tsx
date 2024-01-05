b5dd37cb9ad6277b75afbcb39ba0bd94fda11ad8
Ervin <pangilinanervin22@gmail.com>
2024-01-05 22:03:10 +0800

    Remove global.scss and update RootLayout in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import { Link, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
     return (
-        <div>
-            <div>NavBar</div>
-            <Link to="about">About Us</Link>
-            <Link to="search">Search</Link>
-            <Link to="admin">Admin</Link>
+        <main>
             <Outlet />
-            <div>Footer</div>
-        </div>
+        </main>
     )
 }
