1baf989e3f49e739c2010b1fcdccd174c4d7b1f8
LueKely <99306129+LueKely@users.noreply.github.com>
2024-01-07 11:16:11 +0800

    button style done will fix transition

--- src/pages/Search/SearchHome/SearchHome.tsx
+++ src/pages/Search/SearchHome/SearchHome.tsx
@@ -1,11 +1,34 @@
 import style from './SearchHome.module.scss';
+import { useState } from 'react';
 
 export default function SearchHome() {
+	const [isActive, setIsActive] = useState(true);
+
 	return (
 		<>
 			<h1>ECHOES: Search</h1>
-			<p className="tagline">Simplifying Attendance, One Scan at a Time</p>
-			<p className={style.poop}>lorem burger</p>
+			<p className={style.tagline}>
+				Simplifying Attendance, One Scan at a Time
+			</p>
+			<p className={style.search}>Search by:</p>
+			<div className={style.button_wrapper}>
+				<button
+					className={isActive ? style.student_active : ''}
+					onClick={() => setIsActive(true)}
+				>
+					<div>
+						<p>Student</p>
+					</div>
+				</button>
+				<button
+					className={!isActive ? style.teacher_active : ''}
+					onClick={() => setIsActive(false)}
+				>
+					<div>
+						<p>Teacher</p>
+					</div>
+				</button>
+			</div>
 		</>
 	);
 }
