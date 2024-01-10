e97ead704fbc92326025c586d59b4da07dad9b6a
LueKely <luekely43110@gmail.com>
2024-01-10 08:47:54 +0800

    added wrapper

--- src/pages/Search/SearchResult/SearchResult.tsx
+++ src/pages/Search/SearchResult/SearchResult.tsx
@@ -1,11 +1,12 @@
 import { useSearchParams } from 'react-router-dom';
+import '../Search.scss';
 export default function SearchResult() {
 	const [searchParams] = useSearchParams();
 	const id = searchParams.get('id');
 
 	return (
-		<div>
-			<h1>SearchResult: {id}</h1>
+		<div className="main_wrapper">
+			<h1>Results for: {id}</h1>
 		</div>
 	);
 }
