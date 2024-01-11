6ecd65282306d250dbc8dc467699c6c68788759e
Ervin <pangilinanervin22@gmail.com>
2024-01-11 16:02:06 +0800

    Add Children for Admin: Dashboard and Rooms components

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,8 +1,10 @@
+import { Outlet } from "react-router-dom";
 
 export default function Admin() {
     return (
         <main>
             <h1>Admin</h1>
+            <Outlet />
         </main>
     )
 }
---
+++ src/pages/admin/Rooms/Rooms.tsx
@@ -0,0 +1,6 @@
+
+export default function Rooms() {
+    return (
+        <div>Rooms</div>
+    )
+}
