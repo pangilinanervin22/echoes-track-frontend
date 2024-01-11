31c45f5fd99bb9616a1277185ea5049de5f95a91
Ervin <pangilinanervin22@gmail.com>
2024-01-11 15:38:50 +0800

    Add environment variables for Firebase configuration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,14 @@
 
 interface ImportMetaEnv {
     readonly VITE_SAMPLE: string,
+    VITE_SAMPLE: string;
+    VITE_API_KEY: string;
+    VITE_AUTH_DOMAIN: string;
+    VITE_PROJECT_ID: string;
+    VITE_STORAGE_BUCKET: string;
+    VITE_MESSAGING_SENDER_ID: string;
+    VITE_APP_ID: string;
+    VITE_MEASUREMENT_ID: string;
     // more env variables...
 }
 
