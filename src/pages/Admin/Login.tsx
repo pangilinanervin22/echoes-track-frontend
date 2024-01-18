a1dec2c534ef55dcfef7c7103f96c2f65909eb1e
Ervin <pangilinanervin22@gmail.com>
2024-01-19 01:49:09 +0800

    Refactor login page and rooms page

--- src/pages/Admin/Login.tsx
+++ src/pages/Admin/Login.tsx
@@ -5,6 +5,7 @@ import cvsulogo from '../../assets/images/cvsuLOGO.png'
 import personIcon from '../../assets/images/Vector.png'
 import lockIcon from '../../assets/images/person_FILL0_wght400_GRAD0_opsz24 1.png'
 
+
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -27,29 +28,29 @@ function Login() {
 
     // feel free to change the jsx
     return (
-        <section className={loginStyle.section}>
-                <img src={cvsulogo} alt="" className={loginStyle.logo}/>
+        <main className={loginStyle.mainContainer}>
+            <section className={loginStyle.section}>
+                <img src={cvsulogo} alt="" className={loginStyle.logo} />
                 <h1>Echoes Tracker</h1>
-            <h1 className={loginStyle.h1margin}>Admin</h1>
-            <form onSubmit={handleSubmit}>
+                <h1 className={loginStyle.h1margin}>Admin</h1>
+                <form onSubmit={handleSubmit}>
 
-                <div className={loginStyle.person}>
-                    
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
+                    <div className={loginStyle.person}>
+
+                        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" required />
                         <div className={loginStyle.inputLogo}>
                             <img src={personIcon} alt="" />
                         </div>
-                
 
-                
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
-                <div className={loginStyle.newinputLogo}>
-                            <img src={lockIcon} alt="" className={loginStyle.lockIcon}/>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" required />
+                        <div className={loginStyle.newinputLogo}>
+                            <img src={lockIcon} alt="" className={loginStyle.lockIcon} />
                         </div>
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </section>
+                    </div>
+                    <button type="submit">Login</button>
+                </form>
+            </section>
+        </main>
     );
 }
 
