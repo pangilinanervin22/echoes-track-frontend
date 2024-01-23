15f3035234546239787fc1378f6b438d1ca5c3f3
LueKely <99306129+LueKely@users.noreply.github.com>
2024-01-23 14:54:06 +0800

    Feat: added the card

--- src/Components/Search/SearchCard/CardSearch.tsx
+++ src/Components/Search/SearchCard/CardSearch.tsx
@@ -1,7 +1,26 @@
+import style from './CardSearch.module.scss';
 export default function CardSearch() {
 	return (
-		<div>
-			<h1>CardSearch</h1>
+		<div className={style.SearchCard_wrapper}>
+			<div className={style.img_wrapper}>
+				<img
+					className={style.card_img}
+					src="https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg"
+					alt="photo of"
+				/>
+			</div>
+
+			<div className={style.card_container}>
+				<div className={style.super_wrapper}>
+					<h2>
+						Lue Kely <br /> Anunciacion
+					</h2>
+					<div className={style.info_wrapper}>
+						<p>Current Room:</p>
+						<h1>304</h1>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
--- src/pages/Search/SearchResult/SearchResult.tsx
+++ src/pages/Search/SearchResult/SearchResult.tsx
@@ -1,20 +1,21 @@
 import { useSearchParams, Link } from 'react-router-dom';
 import style from './SearchResult.module.scss';
-
+import CardSearch from '../../../Components/Search/SearchCard/CardSearch';
 export default function SearchResult() {
 	const [searchParams] = useSearchParams();
 	const id = searchParams.get('id');
 
 	return (
-		<section className="result-container">
+		<section className={style['result-container']}>
 			<h1>
 				Results for: <span className={style.accent}>{id}</span>
 			</h1>
-
-			{/* contain za card */}
-			<div className={style.card_wrapper}>test</div>
-
 			<Link to="/search/home">Go back </Link>
+			{/* contain za card */}
+			<div className={style.cards_container}>
+				<CardSearch></CardSearch>
+				<CardSearch></CardSearch>
+			</div>
 		</section>
 	);
 }
