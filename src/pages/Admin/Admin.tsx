9e1508244047238f9a7f203eb24dfb126f7b0022
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-01-31 09:41:19 +0800

    Fix styling issues in LoginStyle and Rooms modules

--- src/pages/Admin/Admin.tsx
+++ src/pages/Admin/Admin.tsx
@@ -1,12 +1,17 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import { firebaseAuth, firebaseDB } from "../../config/firebase";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import Login from "./Login";
 import style from './Admin.module.scss';
+import cvsulogo from '../../assets/images/cvsuLOGO.png'
+import { Icon } from "@iconify/react/dist/iconify.js";
 
 export default function Admin() {
+    const location = useLocation();
+    console.log(location.pathname, location);
+
     const [isAuthorized, setIsAuthorized] = useState(false);
 
     useEffect(() => {
@@ -27,13 +32,35 @@ export default function Admin() {
         <main className={style.admin_container}>
             {isAuthorized &&
                 <nav className={style.admin_nav}>
-                    <Link to="/admin/dashboard"><button>Dashboard</button></Link>
-                    <Link to="/admin/room"><button>Rooms</button></Link>
-                    <Link to="/admin/schedule"><button>Schedules</button></Link>
-                    <Link to="/admin/user"><button>Users</button></Link>
-                    <Link to="/admin/attendance"><button>Attendance</button></Link>
-                    <div>
-                        <button onClick={() => firebaseAuth.signOut()}>Logout</button>
+                    <div className={style.wow}>
+                        <img src={cvsulogo} alt="" />
+                        <h1>Echoes Tracker</h1>
+                    </div>
+                    <div className={style.second}>
+                        <Link to="/admin/dashboard" className={`${style.iconCont} ${location.pathname.startsWith("/admin/dashboard") ? style.active : ''}`}>
+                            <Icon icon="material-symbols:dashboard" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button>Dashboard</button>
+                        </Link>
+                        <Link to="/admin/room" className={`${style.iconCont} ${location.pathname.startsWith("/admin/room") ? style.active : ''}`}>
+                            <Icon icon="cbi:roomsother" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button>Rooms</button>
+                        </Link>
+                        <Link to="/admin/schedule" className={`${style.iconCont} ${location.pathname.startsWith("/admin/schedule") ? style.active : ''}`}>
+                            <Icon icon="ant-design:schedule-filled" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button>Schedules</button>
+                        </Link>
+                        <Link to="/admin/user" className={`${style.iconCont} ${location.pathname.startsWith("/admin/user") ? style.active : ''}`}>
+                            <Icon icon="mdi:user" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button>Users</button>
+                        </Link>
+                        <Link to="/admin/attendance" className={`${style.iconCont} ${location.pathname.startsWith("/admin/attendance") ? style.active : ''}`}>
+                            <Icon icon="icon-park-solid:notes" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button>Attendance</button>
+                        </Link>
+                        <div className={style.iconCont}>
+                            <Icon icon="material-symbols:logout" color="white" width="25px" margin-top="70px" display="flex" align-items="center" justify-content="center" />
+                            <button onClick={() => firebaseAuth.signOut()}>Logout</button>
+                        </div>
                     </div>
                 </nav>
             }
--- src/pages/Admin/Rooms/Rooms.tsx
+++ src/pages/Admin/Rooms/Rooms.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Room, useDeleteRoom, useGetRooms } from "./useRooms";
 import AddRoom from "./AddRoom";
 import EditRoom from "./EditRoom";
+import room from "./Rooms.module.scss"
 
 export default function Room() {
     const { rooms, isLoading } = useGetRooms();
@@ -20,46 +21,48 @@ export default function Room() {
     if (modifyStatus === "isEditing") return <EditRoom room={selectedRoom} changeStatus={setModifyStatus} />
 
     return (
-        <main>
-            <div>
-                <input
-                    type="text"
-                    placeholder="Search..."
-                    value={searchRoomName}
-                    onChange={event => setSearchRoomName(event.target.value)}
-                />
-                <button onClick={() => setModifyStatus("isAdding")}>Add Room</button>
-            </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredRooms.map((room) => (
-                        <tr key={room.id}>
-                            <td>{room.id}</td>
-                            <td>{room.name}</td>
-                            <td>
-                                <button
-                                    onClick={() => {
-                                        setSelectedRoom(room);
-                                        setModifyStatus("isEditing");
-                                    }}>
-                                    Edit
-                                </button>
-                                <button
-                                    onClick={() => { deleteRoom(room.id) }}>
-                                    Delete
-                                </button>
-                            </td>
+        <main className={room.mainContainer}>
+            <div className={room.centered}>
+                <div className={room.inputCont}>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchRoomName}
+                        onChange={event => setSearchRoomName(event.target.value)}
+                    />
+                    <button onClick={() => setModifyStatus("isAdding")}>Add Room</button>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Actions</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {filteredRooms.map((room) => (
+                            <tr key={room.id}>
+                                <td>{room.id}</td>
+                                <td>{room.name}</td>
+                                <td>
+                                    <button
+                                        onClick={() => {
+                                            setSelectedRoom(room);
+                                            setModifyStatus("isEditing");
+                                        }}>
+                                        Edit
+                                    </button>
+                                    <button
+                                        onClick={() => { deleteRoom(room.id) }}>
+                                        Delete
+                                    </button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </main>
     )
 }
