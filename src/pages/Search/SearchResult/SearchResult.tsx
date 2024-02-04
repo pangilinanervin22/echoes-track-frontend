946bfccab1da2572f082542ebd035b7d1b259bd5
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-02-04 16:52:12 +0800

    feat: added functionality from search card

--- src/pages/Search/SearchResult/SearchResult.tsx
+++ src/pages/Search/SearchResult/SearchResult.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect, ChangeEvent } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { Icon } from '@iconify/react';
 
-import firebase from 'firebase/app';
 import 'firebase/database';
 
 import SearchNotFound from '../../../Components/Search/SearchNotFound/SearchNotFound';
@@ -11,61 +10,42 @@ import SearchLoading from '../../../Components/Search/SearchLoading/SearchLoadin
 import { Link } from 'react-router-dom';
 
 import style from './SearchResult.module.scss';
+import { useGetUsers } from '../../Admin/Users/useUsers';
 
 export default function SearchResult() {
 	const [searchParams] = useSearchParams();
 	const id = searchParams.get('id');
-
-	// here is where you put your firebase magic
-	useEffect(() => {
-		// const db = firebase.database();
-		// db.ref(`your-data-path/${id}`)
-		// 	.once('value')
-		// 	.then(snapshot => {
-		// 		const data = snapshot.val();
-		// 		setData(data);
-		// 		setLoading(false);
-		// 	})
-		// 	.catch(error => {
-		// 		console.error('There was an error!', error);
-		// 		setLoading(false);
-		// 	});
-	}, [id]);
-
-	// dummy data
-	interface UserData {
-		fName: string;
-		lName: string;
-		room: string;
-		url: string;
-	}
-	// delete this if you want to pareh
-	const dummyName =
-		'https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg';
-
-	const [data, setData] = useState<UserData[]>([
-		// comment the data below if you want to see the
-		// not found component
-		{ fName: 'Lue Kely', lName: 'Anunigga', room: '302', url: dummyName },
-		{ fName: 'Ervin', lName: 'Panginigga', room: '303', url: dummyName },
-		{ fName: 'Jojo', lName: 'Manigga', room: '304', url: dummyName },
-		{ fName: 'Ernest', lName: 'Montinigga', room: 'CL3', url: dummyName },
-	]);
-	const [loading, setLoading] = useState(true);
-
+	const { users, loading } = useGetUsers();
+	const [ascending, setAscending] = useState(true);
 	const [selectedOption, setSelectedOption] = useState('');
 
 	// handle the changes of the filter
 	const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
 		setSelectedOption(event.target.value);
 		if (event.target.value === 'ascending') {
-			setData(data.sort((a, b) => a.fName.localeCompare(b.fName)));
+			setAscending(true);
+
 		} else if (event.target.value === 'descending') {
-			setData(data.sort((a, b) => b.fName.localeCompare(a.fName)));
+			setAscending(false);
 		}
 	};
 
-	const renderList = data.map((item, index) => {
+	let filteredUsers = users;
+	filteredUsers = users.sort((a, b) => {
+		if (ascending) {
+			return a.name.localeCompare(b.name);
+		} else {
+			return b.name.localeCompare(a.name);
+		}
+	}
+	);
+	if (id) {
+		filteredUsers = users.filter((user) => {
+			return user.name.toLowerCase().includes(id.toLowerCase());
+		});
+	}
+
+	const renderList = filteredUsers.map((item) => {
 		return <CardSearch user={item} />;
 	});
 
@@ -74,13 +54,6 @@ export default function SearchResult() {
 			<h1>
 				Results for: <span className={style.accent}>{id}</span>
 			</h1>
-			<button
-				onClick={() => {
-					setLoading(!loading);
-				}}
-			>
-				Switch for Loading
-			</button>
 			<div className={style.navigation_wrapper}>
 				<Link className={style.back_wrapper} to="/search/home">
 					<div>
