ef459b308a3aa7ff0e9e11d7b3d6f4019169614c
Ervin Pangilinan <pangilinanervin22@gmail.com>
2024-02-05 05:29:42 +0800

    Add CVSU logo and update PWA configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: "Echoes Track",
+        short_name: "PWA",
+        description: "Rfid based attendance system",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "cvsu.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "cvsu.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    })
+  ],
 })
